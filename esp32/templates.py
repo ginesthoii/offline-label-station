b11ba136ba9c3f9e6120f83abc9310e6fec14dfe
Olivia Griffin <oliviagriffindev@gmail.com>
2025-11-26 10:45:01 -0600

    Create templates.py

---
+++ esp32/templates.py
@@ -0,0 +1,21 @@
+def inventory_label():
+    return (
+        "ITEM: Example Item\n"
+        "ID: INV-001\n"
+        "Location: Barn Shelf 3\n"
+        "Category: Tools\n"
+    )
+
+def livestock_label():
+    return (
+        "Animal: Daisy\n"
+        "Species: Goat\n"
+        "Tag: G-004\n"
+        "Birth: 2024-03-12\n"
+    )
+
+def bin_label():
+    return (
+        "BIN: A-14\n"
+        "Contents: Screws / Fasteners\n"
+    )
